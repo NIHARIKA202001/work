7f8b9a3ecdb6934678c461109e3eebb8eb1f7ffd
NIHARIKA202001 <niharika.tiyyagura2001@gmail.com>
2020-03-12 15:40:20 +0530

    Updates

---
+++ precache-manifest.bbd0dd0466057aa8eeaac69443f5fae3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f448ee932a3826f54114809bfbba69e7",
+    "url": "/work/index.html"
+  },
+  {
+    "revision": "d1cb8758779f8330fa93",
+    "url": "/work/static/css/main.5858e240.chunk.css"
+  },
+  {
+    "revision": "df096ca5a630495a01ba",
+    "url": "/work/static/js/2.d8daee36.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/work/static/js/2.d8daee36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1cb8758779f8330fa93",
+    "url": "/work/static/js/main.192e550d.chunk.js"
+  },
+  {
+    "revision": "29ad893b2a5e55fd01d7",
+    "url": "/work/static/js/runtime-main.35c564cc.js"
+  },
+  {
+    "revision": "efe01b90316cc1eca182c240aa38b1f8",
+    "url": "/work/static/media/boy.efe01b90.svg"
+  }
+]);
